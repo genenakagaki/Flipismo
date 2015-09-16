e5babdfc797e92615efedf84af2ba810b33007b2
User <User@gi231lab-09.lehman.edu>
2015-09-16 19:21:00 -0400

    added matchingGame model

--- Flipismo/CardGameViewController.m
+++ Flipismo/CardGameViewController.m
@@ -8,63 +8,56 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchingGame *game;
 
-@property (strong, nonatomic) PlayingCardDeck *playingCardDeck;
-@property (nonatomic) int cardCount;
+//@property (strong, nonatomic) PlayingCardDeck *playingCardDeck;
+//@property (nonatomic) int cardCount;
 
 @end
 
 @implementation CardGameViewController
 
-const int NUM_PLAYING_CARDS = 52;
+- (CardMatchingGame *)game {
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:0 usingDeck:[self createDeck]];
+    return _game;
+}
+
+- (Deck *)deck {
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    }
+    else {
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) { // if deck is not empty
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
+    }
+        
+    self.flipCount++;
+}
 
 - (void)setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-}
-
-- (PlayingCardDeck *)playingCardDeck {
-    if (!_playingCardDeck) {
-        // lazy instantiation
-        _playingCardDeck = [[PlayingCardDeck alloc] init];
-    }
-    return _playingCardDeck;
-}
-
-- (void)setCardCount:(int)cardCount {
-    _cardCount = cardCount;
-}
-
-- (IBAction)touchCardButton:(UIButton *)sender {
-    if (self.cardCount <= NUM_PLAYING_CARDS) {
-        NSLog(@"%d", self.cardCount);
-
-        Card *card;
-        
-        if ([sender.currentTitle length]) {
-            self.cardCount++;
-            
-            if (self.cardCount < 0) {
-                self.cardCount = -1;
-            }
-            
-            [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
-            [sender setTitle:@"" forState:UIControlStateNormal];
-        }
-        else {
-            card = [self.playingCardDeck drawRandomCard];
-            
-            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
-            [sender setTitle:card.contents forState:UIControlStateNormal];
-        }
-        
-        self.flipCount++;
-    }
+    NSLog(@"flipCount changed to %d", self.flipCount);
 }
 
 @end
---
+++ Flipismo/Model/PlayingCardDeck.h
@@ -0,0 +1,15 @@
+//
+//  PlayingCardDeck.h
+//  Flipismo
+//
+//  Created by User on 9/9/15.
+//  Copyright (c) 2015 Lehman College. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+- (instancetype)init;
+
+@end
